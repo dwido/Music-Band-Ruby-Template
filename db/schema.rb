d0af57ad90d0dbda5d22afe5b36e30b949b88017
dwido <dwido@gmail.com>
2012-01-09 16:53:32 -0500

    wow! this is the user password, encryption and user model stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120108213243) do
+ActiveRecord::Schema.define(:version => 20120109214850) do
 
   create_table "bios", :force => true do |t|
     t.string   "name"
@@ -79,6 +79,10 @@ ActiveRecord::Schema.define(:version => 20120108213243) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
