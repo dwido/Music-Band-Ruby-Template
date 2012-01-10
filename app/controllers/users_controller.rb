f3068caff1523b1f49769ee942db97675c8fed4c
dwido <dwidob@gmail.com>
2012-01-10 12:01:58 -0500

    user signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,15 +41,12 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(params[:user])
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to @user
+      flash[:success] = "Welcome to the Sample App!"
+    else
+      @title = "Sign up"
+      render 'new'
     end
   end
 
