cc4cd6407b6a4ffd26125289f3d67f8bebec6fb0
dwido <dwidob@gmail.com>
2012-01-14 19:05:03 -0500

    new images for home page

--- public/javascripts/initgallery.js
+++ public/javascripts/initgallery.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-    $('#coin-slider').coinslider({ width: 900, height: 400, navigation: false, delay: 2000, effect:  'straight'});
+    $('#coin-slider').coinslider({ width: 900, height: 390, navigation: false, delay: 2000, effect:  'straight'});
 });
