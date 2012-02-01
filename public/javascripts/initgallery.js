a39bfcba5bf8c7f0935fff3b5198dc729fe908f7
dwido <dwidob@gmail.com>
2012-01-31 22:08:18 -0500

    face lift to the application

--- public/javascripts/initgallery.js
+++ public/javascripts/initgallery.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-    $('#coin-slider').coinslider({ width: 900, height: 390, navigation: false, delay: 2000, effect:  'straight'});
+    $('#coin-slider').coinslider({ width: '100%', height: 390, navigation: false, delay: 2000, effect:  'straight'});
 });
